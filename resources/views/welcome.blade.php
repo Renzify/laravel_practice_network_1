af4a44ebd3547e317b82e934033ac140138d01c0
Renz <168178279+Renzify@users.noreply.github.com>
2025-03-23 06:33:39 +0800

    Adding CSS and Tailwind

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,13 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Practice Network</title>
+    @vite('resources/css/app.css')
 </head>
 
-<body>
+<body class="text-center px-8 py-12">
     <h1>Welcome to Practice Network</h1>
     <p>Click the button below to view the list of members.</p>
 
-    <a href="/members" class="button">
+    <a href="/members" class="btn">
         Find Members
     </a>
 </body>
